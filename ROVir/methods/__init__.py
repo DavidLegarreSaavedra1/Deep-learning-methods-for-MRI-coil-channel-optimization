cbee523deb9420451f77e2c7a26add7327bbaacf
ls929 <davidls920@gmail.com>
2022-04-24 14:34:49 +0200

    Final solution

--- ROVir/methods/__init__.py
+++ ROVir/methods/__init__.py
@@ -2,5 +2,5 @@ from ..methods.ROVir import ROVir
 from ..methods.image_manipulation import *
 from ..methods.matrix_manipulation import *
 from ..methods.HDF5 import *
-from ..methods.ROVIR_im import *
+from ..methods.ROVir_im import *
 from ..methods.complex import *
