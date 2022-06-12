FastNN fixed, removed normalization, since images are already normalized

NNs/main.py
@@ -18,8 +18,8 @@ plt.rcParams["savefig.bbox"] = "tight"
 torch.cuda.empty_cache()
 
 
-N_EPOCHS = 100
-BATCH_SIZE = 32
+N_EPOCHS = 50
+BATCH_SIZE = 4
 IMG_SIZE = 96
 TO_TRAIN = True
 
@@ -42,16 +42,16 @@ if __name__ == '__main__':
    
     heart_dataset = ChestHeartDataset(
         root_data_path, training_ann, 
-        transforms=train_transformers
+        transforms=None
     )
 
 
     train_len = int(0.9 * len(heart_dataset))
-    test_len = int((len(heart_dataset)-train_len)/2)
-    val_len = int(len(heart_dataset)-train_len-test_len)
+    #test_len = int((len(heart_dataset)-train_len)/2)
+    val_len = int(len(heart_dataset)-train_len)
 
     train_dataset, val_dataset= torch.utils.data.random_split(
-        heart_dataset, [train_len, val_len*2]
+        heart_dataset, [train_len, val_len]
     )
 
     training_data_loader = torch.utils.data.DataLoader(

NNs/methods/dataset.py
@@ -49,12 +49,12 @@ class ChestHeartDataset(Dataset):
         img_path = img_path[:6] + img_path[8:]
         img_path = self.root + '/' + img_path
         img = cv.imread(img_path, 0)
-        img = auto_contrast(img)
+        #img = auto_contrast(img)
         img = img.astype(np.float)
         w,h = img.shape[:2]
-        _, bins = np.histogram(img.flatten(), bins=64)
-        limit = np.quantile(bins, 0.8)
-        img /= limit
+        #_, bins = np.histogram(img.flatten(), bins=64)
+        #limit = np.quantile(bins, 0.8)
+        #img /= limit
         #img = Image.open(img_path)
         tensorizer = T.Compose([
             T.ToTensor(),

NNs/methods/neural_network/FastNN.py
@@ -48,19 +48,15 @@ class FastNN(nn.Module):
             nn.Conv2d(1, 10, 3),
             nn.BatchNorm2d(10),
             nn.ReLU(),
+            nn.AvgPool2d(2)
         )
 
         self.block2 = nn.Sequential(
-            nn.Conv2d(10, 24, 3),
+            nn.Conv2d(10, 24, 3, stride=2),
             nn.BatchNorm2d(24),
             nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(24, 24, 3),
+            nn.Conv2d(24, 24, 3, stride=2),
             nn.BatchNorm2d(24),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(24, 32, 3),
-            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2),
             nn.Dropout(0.5)
@@ -68,14 +64,15 @@ class FastNN(nn.Module):
 
         self.box_regressor = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(10*94*94, 4),
-            nn.Dropout(0.5)
+            nn.Linear(24*5*5, 4),
+            #nn.Dropout(0.5)
         )
     
     def forward(self, x: torch.Tensor):
         x = self.resize(x)
 
         x = self.block1(x)
+        x = self.block2(x)
         x = self.box_regressor(x)
 
         return x

NNs/methods/neural_network/Train.py
@@ -1,5 +1,4 @@
 from tqdm.auto import tqdm
-from torch.utils.tensorboard import SummaryWriter
 from logging import root
 from datetime import datetime
 from timeit import default_timer as timer
@@ -19,8 +18,8 @@ def loss_fn(pred, target):
     #target /= 144
     # print(f"{pred=}")
     # print(f"{target=}")
-    loss = nn.SmoothL1Loss()(pred, target)
-    #loss = nn.L1Loss()(pred, target)
+    #loss = nn.SmoothL1Loss()(pred, target)
+    loss = nn.L1Loss()(pred, target)
     #loss = nn.MSELoss()(pred, target)
     return loss
 
@@ -90,13 +89,13 @@ def train(
     loss_saved = []
     best_epoch = []
     best_vloss = 1_000_000
-    patience = 7
+    patience = 5
     trigger = 0
 
     adam = False
 
     optimizer = torch.optim.SGD(
-        model.parameters(), lr=3e-2,
+        model.parameters(), lr=1e-3,
         momentum=0.9, weight_decay=1e-2
     )
 

NNs/methods/visualization.py
@@ -33,11 +33,11 @@ def preprocess(img, device = "cpu", img_size = 144):
     """
 
     image = cv.resize(img, (img_size, img_size))
-    image = auto_contrast(img, 0.99)
-    hist, bins = np.histogram(image.flatten(), bins=64)
-    image = image.astype(np.float)
-    limit = np.quantile(bins, 0.8)
-    image /= limit
+    #image = auto_contrast(img, 0.99)
+    #hist, bins = np.histogram(image.flatten(), bins=64)
+    #image = image.astype(np.float)
+    #limit = np.quantile(bins, 0.8)
+    #image /= limit
     #image = image.astype(np.float) / 255.0
 
     # convert image to a tensor